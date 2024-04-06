0f2706bfc55ae49b65789dc730f694bbbd0a38ad
Tarsh Vaibhav <tbhaiv@gmail.com>
2024-04-06 11:15:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// Import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+//const morgan = require('morgan');
+
+// Create an Express application
+const app = express();
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware for logging HTTP requests
+//app.use(morgan('dev'));
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/tarsh', (req, res) => {
+    res.send('Hello, Tarsh');
+  });
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
